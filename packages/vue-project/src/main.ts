1f2524e027138b5d3b8b7dbbf03cf3a39544c902
nicky132 <hhq87776153@hotmail.com>
2023-07-16 15:54:03 +0800

    解决主应用路由不匹配后产生path为undefined得bug

--- packages/vue-project/src/main.ts
+++ packages/vue-project/src/main.ts
@@ -1,27 +1,35 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { registerMicroApps, start } from 'qiankun';
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { registerMicroApps, start } from "qiankun";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+//主应用使用的嵌套路由
+router.beforeEach((to, from, next) => {
+  if (!window.history.state.current) window.history.state.current = to.fullPath;
+  if (!window.history.state.back) window.history.state.back = from.fullPath;
+  // 手动修改history的state
+  return next();
+});
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
 
 // 注册子应用
 registerMicroApps([
   {
-    name: 'react-app',
-    entry: '//localhost:7100',
-    container: '#subapp-container',
-    activeRule: '/react',
+    name: "react-app",
+    entry: "//localhost:7100",
+    container: "#subapp-container",
+    activeRule: "/react",
   },
-])
+]);
 
 // 启动 qiankun
-start()
+start();
